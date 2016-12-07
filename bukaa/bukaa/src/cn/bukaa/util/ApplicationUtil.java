91003d44b4858eab51214977d89404aaec3ffa70
bukaa <940446879@qq.com>
2016-12-07 19:19:39 +0800

    新增工具类和测试方法

---
+++ bukaa/bukaa/src/cn/bukaa/util/ApplicationUtil.java
@@ -0,0 +1,21 @@
+package cn.bukaa.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class ApplicationUtil implements ApplicationContextAware {
+	
+	private static ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		ApplicationUtil.applicationContext = applicationContext;
+	}
+	
+	public static Object getBean(String beanName){
+		return applicationContext.getBean(beanName);
+	}
+
+}
